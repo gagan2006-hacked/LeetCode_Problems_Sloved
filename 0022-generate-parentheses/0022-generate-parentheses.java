d75061855e8ba1af23649310af1ea8a19b4ae269
Gagan V <gaganvishwanath2006@gmail.com>
2026-06-22 15:48:58 +0530

    Time: 1 ms (86.77%), Space: 42.5 MB (99.99%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        return genpara(n);
+    }
+    public static List<String> genpara(int n) {
+        StringBuilder value = new StringBuilder();
+        List<String> list=new ArrayList<>();
+        genparaHelper(value, n, 0, 0,list);
+        return list;
+    }
+
+    private static void genparaHelper(StringBuilder value, int n, int openValue, int closeValue,List<String> result) {
+        if (value.length() == 2 * n) {
+            result.add(value.toString());
+            return;
+        }
+        if (openValue < n) {
+            value.append("(");
+            genparaHelper(value, n, openValue + 1, closeValue,result);
+            value.deleteCharAt(value.length() - 1);
+        }
+        if (closeValue < openValue) {
+            value.append(")");
+            genparaHelper(value, n, openValue, closeValue + 1,result);
+            value.deleteCharAt(value.length() - 1);
+        }
+    }
+}
