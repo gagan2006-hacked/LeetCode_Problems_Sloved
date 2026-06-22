a11908aadd8a2a86496a3721af920ceed6c86bf5
Gagan V <gaganvishwanath2006@gmail.com>
2026-06-22 15:48:35 +0530

    Time: 3 ms (87.55%), Space: 43.5 MB (25.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.isBlank()){
+            return true;
+        }
+        Stack<Character>stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if (c=='('||c=='['||c=='{'){
+                stack.push(c);
+            }else {
+                if (c==')'){
+                    if (stack.isEmpty()){
+                        return false;
+                    }
+                    if (stack.peek()=='('){
+                        stack.pop();
+                    }else {
+                         return false;
+                    }
+                } else if (c==']') {
+                    if (stack.isEmpty()){
+                        return false;
+                    }
+                    if (stack.peek()=='['){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }else if (c=='}'){
+                    if (stack.isEmpty()){
+                        return false;
+                    }
+                    if (stack.peek()=='{'){
+                        stack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
