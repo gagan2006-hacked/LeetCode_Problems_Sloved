ec22aa4ec918b8af2dc1c9d65f429378dd380a3f
Gagan V <gaganvishwanath2006@gmail.com>
2026-06-22 15:46:07 +0530

    Time: 2 ms (99.8%), Space: 44.7 MB (99.99%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static String convert(String s, int numRows) {
+        if (numRows <= 1 || s.length() <= numRows) {
+            return s;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        int cycleLen = 2 * (numRows - 1);
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = i; j < s.length(); j += cycleLen) {
+                builder.append(s.charAt(j));
+
+                // middle rows need an extra character in between
+                int secondJ = j + cycleLen - 2 * i;
+                if (i > 0 && i < numRows - 1 && secondJ < s.length()) {
+                    builder.append(s.charAt(secondJ));
+                }
+            }
+        }
+
+        return builder.toString();
+    }
+}
